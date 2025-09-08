4414940ed6dfecc787a58150714deda1453995ec
Priyansh Mishra <priyanshmishra801@gmail.com>
2025-09-08 14:21:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-// config.js
 import { createClient } from '@supabase/supabase-js'
 
-// Supabase Project URL और anon/public key डालो
 const supabaseUrl = "https://xnuondpfihckkqzyntjv.supabase.co"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...."  // तुमारी anon/public key
 export const supabase = createClient(supabaseUrl, supabaseKey)
